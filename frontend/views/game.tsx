3cc0d6bdd5a4a3fbcbb5cf1422802df5a2b09d9b
Jack O'Brien <thejackobrien29@gmail.com>
2023-08-31 21:51:27 -0400

    update the game

--- frontend/views/game.tsx
+++ frontend/views/game.tsx
@@ -42,6 +42,7 @@ const Game: React.FC<Props> = ({}) => {
   const [options, setOptions] = useState<string[]>([]);
   const [correctGuesses, setCorrectGuesses] = useState(0);
   const [streak, setStreak] = useState(0);
+  const [showFullName, setShowFullName] = useState(false);
   const toast = useToast();
 
   useEffect(() => {
@@ -61,13 +62,12 @@ const Game: React.FC<Props> = ({}) => {
       toast.success('Woohoo! You got it right!');
     } else {
       setStreak(0);
-      toast.error(`Oh no, that's not right. Luckily it's just practice!`);
+      toast.error(`Oh no, that's not right. That's a picture of ${allPeople[index].names[0]}!!`);
     }
 
     setIndex((index + 1) % allPeople.length);
   };
 
-  console.log(allPeople[index].imageUrl);
   return (
     <Flex align="center" flexDir="column" w="full" pt={[4, 4, 8]}>
       <Heading fontSize={['2xl', '2xl', '4xl']}>Mediterranean Name Game</Heading>
@@ -86,12 +86,15 @@ const Game: React.FC<Props> = ({}) => {
         {options.map((option: any, idx: number) => {
           return (
             <Button w="full" key={option} onClick={() => onGuess(idx)} colorScheme="green">
-              {option}
+              {showFullName ? option : option.split(' ')[0]}
             </Button>
           );
         })}
       </Flex>
-      Use Full Name <Switch />
+
+      <Flex mt={4} gap={2}>
+        Show Full Name <Switch onChange={(e) => setShowFullName(e.target.checked)} />
+      </Flex>
     </Flex>
   );
 };
